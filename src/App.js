15c3b1520b73dbc1068affcc26aa776df4288ecd
imran82ali <code.imranali@gmail.com>
2021-06-04 14:14:54 +0500

    ShoeStore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Header   from './components/Header'
+import Home     from './components/Home'
+import Products from './components/Products';
+import ProductDetails from './components/ProductDetails';
+import ProductIndex from './components/ProductIndex';
+import NotFound from './components/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+    <h1> Hammad's Foot Wear </h1>
+    <Routes>
+      <Route path="/"      element={<Home/>}> </Route>
+      <Route path="products"  element={<Products/>}> 
+
+        <Route path="/"       element={<ProductIndex />}> </Route>
+        <Route path=":productID" element={<ProductDetails />}> </Route>
+
+      </Route>
+      <Route path="*"         element={<NotFound/>}> </Route>
+    </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom';
+
+
+function Products() {
+    return (
+      <div>
+        <h1> Products </h1>
+        <Outlet/>
+      </div>
+    );
+  }
+
+export default Products;
